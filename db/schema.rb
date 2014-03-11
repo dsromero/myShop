8f5c61e33cff2eebb4c50db1d8b13cf79a5363a5
dsromero <chnmls@gmail.com>
2014-03-11 18:31:55 +0100

    modelo LineItem creado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311164022) do
+ActiveRecord::Schema.define(version: 20140311172710) do
+
+  create_table "line_items", force: true do |t|
+    t.string   "product"
+    t.integer  "amount"
+    t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "orders", force: true do |t|
     t.string   "status"
