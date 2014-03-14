5824e9dd5ea3547fdd334d8609e4f6becec31670
dsromero <chnmls@gmail.com>
2014-03-14 18:44:12 +0100

    relaciones creadas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314171330) do
+ActiveRecord::Schema.define(version: 20140314173012) do
 
   create_table "line_items", force: true do |t|
     t.string   "product"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140314171330) do
     t.string   "second_name_buyer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "line_item_id"
   end
 
   create_table "products", force: true do |t|
